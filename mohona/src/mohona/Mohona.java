f721ceb9fce98ec8ccab727fa791ca7546df7764
Nabhan Abdullah <89448502+NabhanJS@users.noreply.github.com>
2021-10-14 17:25:15 +0200

    Add files via upload

---
+++ mohona/src/mohona/Mohona.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mohona;
+
+/**
+ *
+ * @author nabha
+ */
+public class Mohona {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int vaccin=0;
+        while (vaccin <10){
+            vaccin+=2;
+            System.out.println(vaccin);
+        
+        }
+      
+    }
+    
+}
